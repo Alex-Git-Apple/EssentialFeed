5ef452a744f87fe522b0680e2a26f5538d2f320e
Pin <just4sport30@gmail.com>
2023-05-31 21:50:09 -0700

    Add more tests for different status code.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -59,14 +59,15 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
-        
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load() { capturedErrors.append($0) }
-        
-        let clientError = NSError(domain: "Test", code: 0)
-        client.complete(withStatusCode: 400)
-        
-        XCTAssertEqual(capturedErrors, [.invalidData])
+    
+        [199, 201, 300, 400, 500].enumerated().forEach { index, code in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load() { capturedErrors.append($0) }
+            
+            client.complete(withStatusCode: code, at: index)
+            
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
     
     private class HTTPClientSpy: HTTPClient {
@@ -94,5 +95,4 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(response, nil)
         }
     }
-    
 }
