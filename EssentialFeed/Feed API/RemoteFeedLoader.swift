54be515a2c224c3a9d0f9d3bb5900249326d9349
Pin <just4sport30@gmail.com>
2023-06-01 17:22:41 -0700

    More clean up on FeedItemMapper.

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -53,28 +53,30 @@ public final class RemoteFeedLoader {
 }
 
 private class FeedItemsMapper {
+    private struct Root: Codable {
+        var items: [Item]
+    }
+
+    private struct Item: Codable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+    
+    static let OK_200 = 200
+    
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         let root = try JSONDecoder().decode(Root.self, from: data)
         
         return root.items.map{ $0.item }
-    }
-}
-
-private struct Root: Codable {
-    var items: [Item]
-}
-
-private struct Item: Codable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-    
-    var item: FeedItem {
-        return FeedItem(id: id, description: description, location: location, imageURL: image)
     }
 }
 
